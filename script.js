76b312be4c4dc35732ce08d08f9257deca15a052
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-03-16 01:30:24 -0400

    Add game function and adjust playRound function to feed score to game

--- script.js
+++ script.js
@@ -19,29 +19,53 @@ function playRound(playerSelection, computerChoice) {
     win = `You win! Your ${playerSelection} beats Computer's ${computerChoice}!`;
     lose = `You lose! Computer's ${computerChoice} beats your ${playerSelection}!`;
     tie = `It's a tie! Play again.`;
-    badInput = "Not an option, buddy.";
+    badInput = "Not an option, buddy. Round forfeit to Computer.";
 
     if (playerSelection === computerChoice) {
-        return tie;
+        console.log(tie);
+        return 0;
     } else if (playerSelection === "rock") {
         if (computerChoice === "scissors") {
-            return win;
+            console.log(win);
+            return 1;
         } else {
-            return lose;
+            console.log(lose);
+            return -1;
         }
     } else if (playerSelection === "paper") {
         if (computerChoice === "rock") {
-            return win;
+            console.log(win);
+            return 1;
         } else {
-            return lose;
+            console.log(lose);
+            return -1;
         }
     } else if (playerSelection === "scissors") {
         if (computerChoice === "paper") {
-            return win;
+            console.log(win);
+            return 1;
         } else {
-            return lose;
+            console.log(lose);
+            return -1;
         }
     } else {
-        return badInput;
+        console.log(badInput);
+        return -1;
     }
+}
+
+function game() {
+
+    console.log("Let's play.")
+
+    let score = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt("Rock, paper, or scissors?");
+        score = score + playRound(playerSelection);
+        console.log(`The score is now ${score}.`);
+    }
+
+    console.log(`Your final score is ${score}. Thanks for playing!`);
+
 }
