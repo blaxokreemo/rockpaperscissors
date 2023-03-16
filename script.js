02a3a9c753e3fc21bf99e29b4b44f207ee4ab53e
blaxokreemo <126304806+blaxokreemo@users.noreply.github.com>
2023-03-15 23:21:08 -0400

    Add getComputerChoice function

---
+++ script.js
@@ -0,0 +1,13 @@
+function getComputerChoice() {
+    let randomNumber = (Math.random() * 100);
+    let computerChoice;
+    if (randomNumber <= 33) {
+        computerChoice = "rock";
+    } else if (randomNumber >= 33 && randomNumber <= 66) {
+        computerChoice = "paper";
+    } else {
+        computerChoice = "scissors";
+    }
+
+    return computerChoice;
+}
